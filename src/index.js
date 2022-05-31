a44266f6419ba81973ee4eaca5f413f816deb4f4
wangjp <943375372@qq.com>
2022-05-31 20:28:24 +0800

    升级依赖

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import App from './App.vue';
-import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/dist/index.css';
 
 const app = createApp(App);
 
